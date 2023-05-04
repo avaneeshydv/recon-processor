010fd5c93fbcc233c2c265458106fb7a576643f4
avaneesh.yadav <avaneesh.yadav@payu.in>
2023-05-04 18:11:27 +0530

    dependency fix

--- src/main/java/com/recon/reconprocessor/controller/ReconController.java
+++ src/main/java/com/recon/reconprocessor/controller/ReconController.java
@@ -1,5 +1,14 @@
 package com.recon.reconprocessor.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class ReconController {
+
+  @GetMapping("/test")
+  ResponseEntity<String> getTest() {
+    ResponseEntity.ok("success");
+  }
 }
