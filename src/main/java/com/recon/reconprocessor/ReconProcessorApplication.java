5466fccccc3c5aa3d115c306983fd69fcdb89d58
avaneesh.yadav <avaneesh.yadav@payu.in>
2023-05-04 17:35:55 +0530

    first commit

---
+++ src/main/java/com/recon/reconprocessor/ReconProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.recon.reconprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReconProcessorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ReconProcessorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/recon/reconprocessor/ReconProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recon.reconprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReconProcessorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
